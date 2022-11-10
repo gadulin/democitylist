263c1471aecb6f822d3ae8d1104a6f5810353fd1
Stanislav Milev <gadulin@gmail.com>
2022-11-10 20:01:30 +0200

    generated

---
+++ democitylist-server/src/main/java/io/github/gadulin/democitylist/DemocitylistApplication.java
@@ -0,0 +1,13 @@
+package io.github.gadulin.democitylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocitylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocitylistApplication.class, args);
+	}
+
+}
---
+++ democitylist-server/src/test/java/io/github/gadulin/democitylist/DemocitylistApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gadulin.democitylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocitylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
